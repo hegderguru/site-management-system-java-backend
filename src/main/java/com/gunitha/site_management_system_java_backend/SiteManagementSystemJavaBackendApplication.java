060ff0f147fb5f46e999c83b42fe096de1549925
hegderguru <hegde.r.guru@gmail.com>
2025-01-19 13:59:55 +0530

    initial

---
+++ src/main/java/com/gunitha/site_management_system_java_backend/SiteManagementSystemJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gunitha.site_management_system_java_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteManagementSystemJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteManagementSystemJavaBackendApplication.class, args);
+	}
+
+}
