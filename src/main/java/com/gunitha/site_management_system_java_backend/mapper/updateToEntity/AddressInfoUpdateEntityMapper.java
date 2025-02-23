c379ad32dd695e62c5c1d877e808256fa9583208
hegderguru <hegde.r.guru@gmail.com>
2025-02-23 11:16:15 +0530

    initial

---
+++ src/main/java/com/gunitha/site_management_system_java_backend/mapper/updateToEntity/AddressInfoUpdateEntityMapper.java
@@ -0,0 +1,17 @@
+package com.gunitha.site_management_system_java_backend.mapper.updateToEntity;
+
+import com.gunitha.site_management_system_java_backend.change.model.Change;
+import com.gunitha.site_management_system_java_backend.entity.Address;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class AddressInfoUpdateEntityMapper {
+
+    public void updateAddressEntity(List<Change> changes, Address address){
+
+    }
+}
