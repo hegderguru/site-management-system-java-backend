4c888b9a37356514ce93b4fe43c44d42625fa0a9
Guru Raghavendra Hegde <hegde.r.guru@gmail.com>
2025-02-23 16:48:09 +0530

    initial

--- src/main/java/com/gunitha/site_management_system_java_backend/repository/IPersonRepository.java
+++ src/main/java/com/gunitha/site_management_system_java_backend/repository/IPersonRepository.java
@@ -1,7 +1,6 @@
 package com.gunitha.site_management_system_java_backend.repository;
 
 import com.gunitha.site_management_system_java_backend.entity.Person;
-import com.gunitha.site_management_system_java_backend.model.read.PersonInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +11,4 @@ public interface IPersonRepository extends JpaRepository<Person,Long> {
 
     List<Person> findPersonsByOrganisationsId(Long id);
 
-    List<Person> findPersonBySitesId(Long siteId);
 }
